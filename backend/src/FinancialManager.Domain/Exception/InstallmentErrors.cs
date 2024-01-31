6820e216564421e4b422a87277a3727be16ec722
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-01-31 11:55:37 -0300

    chore: errors code

--- backend/src/FinancialManager.Domain/Exception/InstallmentErrors.cs
+++ backend/src/FinancialManager.Domain/Exception/InstallmentErrors.cs
@@ -3,6 +3,7 @@
 namespace FinancialManager.Domain.Exception;
 public static class InstallmentErrors
 {
-    public static readonly Error NotFound = Error.NotFound("Not.Found", "Installment was not found");
-    public static readonly Error InvalidAmount = Error.Validation("Amount.Less.Equal.Zero", "The amount of the installment should be greatter than zero");
+    public static readonly Error NotFound = Error.NotFound("Installment.Not.Found", "Installment was not found");
+    public static readonly Error InvalidAmount = Error.Validation("Installment.Amount.Less.Equal.Zero", "The amount of the installment should be greatter than zero");
+    public static readonly Error InvalidInstallment = Error.Validation("Installment.invalid.amount", "The installment amount is greatter then transaction value remaining to pay");
 }
--- backend/src/FinancialManager.Domain/Exception/TransactionErrors.cs
+++ backend/src/FinancialManager.Domain/Exception/TransactionErrors.cs
@@ -3,9 +3,9 @@
 namespace FinancialManager.Domain.Exception;
 public static class TransactionErrors
 {
-    public static readonly Error InvalidType = Error.Validation("Invalid.TransactionType", "Invalid Transaction Type");
-    public static readonly Error NotFound = Error.NotFound("Not.Found", "Transaction was not found");
-    public static readonly Error TransactionDoesNotAcceptInstallment = Error.Validation("CannotAdd.Installment", "Debit transaction can not add installment");
-    public static readonly Error InvalidInstallment = Error.Validation("Installment.invalid.amount", "The installment amount is greatter then transaction value remaining to pay");
+    public static readonly Error InvalidType = Error.Validation("Invalid.Transaction.Type", "Invalid Transaction Type");
+    public static readonly Error NotFound = Error.NotFound("Transaction.Not.Found", "Transaction was not found");
+    public static readonly Error TransactionDoesNotAcceptInstallment = Error.Validation("Cannot.Add.Installment", "Debit transaction can not add installment");
+    
 }
 
